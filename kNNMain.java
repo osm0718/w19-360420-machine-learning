1f57f5b9cbf37c51383f0517a26bce158953c93b
Oliver Miller <osm0718@Olivers-MacBook-Pro-10.local>
2019-05-08 15:28:24 -0400

    Task0

--- kNNMain.java
+++ kNNMain.java
@@ -10,7 +10,11 @@ public class kNNMain{
 
     // TASK 1: Use command line arguments to point DataSet.readDataSet method to
     // the desired file. Choose a given DataPoint, and print its features and label
+    DataSet pt = new DataSet();
 
+    pt.readDataSet(args[0]);
+
+    printDataSet(pt);
 
 
     //TASK 2:Use the DataSet class to split the fullDataSet into Training and Held Out Test Dataset
